bc1d8dff9f074c5705714b6e5a2c765660d879f6
Kalidyasin1 <kalidy722@gmail.com>
2026-08-13 00:27:19 +0300

    Create useAuthStore.ts

---
+++ src/src/src/src/src/types/src/stores/src/stores/useAuthStore.ts
@@ -0,0 +1,20 @@
+import create from 'zustand'
+
+interface AuthState {
+  user: string | null
+  login: (u: string, p: string) => Promise<boolean>
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  login: async (u, p) => {
+    // Hardcoded admin
+    if (u === 'kalido' && p === 'kalido2026') {
+      set({ user: u })
+      return true
+    }
+    return false
+  },
+  logout: () => set({ user: null })
+}))
