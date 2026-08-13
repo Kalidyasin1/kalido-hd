17448824ea4a82c6a7cba352d6becf199c2a2a5b
Kalidyasin1 <kalidy722@gmail.com>
2026-08-13 22:56:45 +0300

    Create useThemeStore.ts

---
+++ src/stores/useThemeStore.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+
+type ThemeId = 'black-gold'|'dark-blue'|'purple'|'green'|'red'|'light'
+
+interface ThemeState {
+  id: ThemeId
+  setTheme: (id: ThemeId) => void
+}
+
+const initial = (localStorage.getItem('kalido-theme') as ThemeId) || 'black-gold'
+
+export const useThemeStore = create<ThemeState>((set) => ({
+  id: initial,
+  setTheme: (id) => {
+    localStorage.setItem('kalido-theme', id)
+    document.body.setAttribute('data-theme', id === 'black-gold' ? '' : id === 'light' ? 'light' : id.replace('-', ''))
+    set({ id })
+  }
+}))
